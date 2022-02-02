4c3d0692967f07344926a0e1c501e7ad08d0ceb4
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-02-01 21:47:24 -0300

    Configurando para sempre gerar CPFs valido. OBS.: Esses CPFs não estão cadastrados em nenhum sistema do governo. São apenas fiquiticios

--- validador_CPF.py
+++ validador_CPF.py
@@ -1,33 +1,30 @@
+# Gerando CPFs validos
+from random import randint
+
 while True:
-  # cpf gerado aleatoriamente
-  # cpf = '16899535009'
-  cpf = input('Digite o CPF: ')
-  novo_cpf = cpf[:-2]
-  reverso = 10
+  numero = str(randint(100000000, 999999999))
+
+  novo_cpf = numero              # Elimina os dois últimos dígitos do CPF
+  reverso = 10                      # Contador reverso                     
   total = 0
+  # Loop do CPF
   for index in range(19):
-      if index > 8:
-        index -= 9
+      if index > 8:              # Primeiro índice de 0 a 9              
+        index -= 9               # São os 9 primeiros dígitos do CPF
 
-      total += int(novo_cpf[index]) * reverso
+      total += int(novo_cpf[index]) * reverso     # Valor total da multiplicação
 
-      reverso -= 1
+      reverso -= 1                  # Decrementa o contador reverso 
       if reverso < 2:
           reverso = 11
           d = 11 - (total % 11)  
         
-          if d > 9:
+          if d > 9:                 # caso o digito for > que 9 o valor é 0
               d = 0
-          total = 0
-          novo_cpf += str(d)  
+          total = 0                 # zera o total
+          novo_cpf += str(d)        # concatena o digito calculado ao CPF
 
-  if cpf == novo_cpf:
-    print('Válido')
-  else:
-    print('Inválido')
+  print(novo_cpf)
 
-  print('Deseja continuar? (S/N)')
-  resposta = input()
-
-  if resposta in 'Nn':
+  if input('Deseja gerar outro CPF? [S/N] ').upper() == 'N':
     break
